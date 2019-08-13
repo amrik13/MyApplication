1eb5c3be796be36d2250b6e2fda6dbecc174ab1c
amrik13 <amrik5188@gmail.com>
2019-08-13 11:55:14 +0530

    third commit

--- app/src/main/java/com/amriksinghpadam/multi_recycler_view1/GridViewClass.java
+++ app/src/main/java/com/amriksinghpadam/multi_recycler_view1/GridViewClass.java
@@ -6,12 +6,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.Cursor;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class GridViewClass extends AppCompatActivity {
+    TextView boyHead,girlHead;
     RecyclerView recyclerView,recyclerView2;
     LinearLayout layout;
     ArrayList<String> imgList = new ArrayList<>();
@@ -25,11 +30,19 @@ public class GridViewClass extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_view_class);
 
+        boyHead = findViewById(R.id.boyHeadId);
+        girlHead = findViewById(R.id.girlHeadId);
+
+        boyHead.setText("BOY");
+        boyHead.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+        girlHead.setText("GIRL");
+        girlHead.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+
         MyDatabase db = new MyDatabase(this);
         Cursor data = db.getAllData();
 
         while(data.moveToNext()){
-            imgList.add("https://image.shutterstock.com/image-vector/business-man-icon-260nw-1009389559.jpg");
+            imgList.add("https://cdn2.iconfinder.com/data/icons/men-avatars/33/man_19-512.png");
             textList.add(data.getString(1));
         }
 
@@ -43,7 +56,7 @@ public class GridViewClass extends AppCompatActivity {
         textList2.add("User 4");
         imgList2.add("https://static.thenounproject.com/png/183640-200.png");
         textList2.add("User 5");
-        imgList2.add("https://static.thenounpro ject.com/png/183640-200.png");
+        imgList2.add("https://static.thenounproject.com/png/183640-200.png");
         textList2.add("User 6");
         imgList2.add("https://static.thenounproject.com/png/183640-200.png");
         textList2.add("User 7");
@@ -55,11 +68,12 @@ public class GridViewClass extends AppCompatActivity {
             recyclerView = findViewById(R.id.recyclerViewId);
             recyclerView2 = findViewById(R.id.recyclerViewId2);
             layout = findViewById(R.id.layoutId);
+// first recycler view adapter
             AdapterClass adapter = new AdapterClass(imgList,textList,this);
             recyclerView.setAdapter(adapter);
             //recyclerView.setLayoutManager(new GridLayoutManager(this,2));
             recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
-
+// second recycler view adapter
             AdapterClass adapter2 = new AdapterClass(imgList2,textList2,this);
             recyclerView2.setAdapter(adapter2);
             //recyclerView.setLayoutManager(new GridLayoutManager(this,2));
